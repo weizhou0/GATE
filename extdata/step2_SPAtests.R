422515c9d47bc3da446749fc1ace6af5936364f6
weizhouUMICH <zhowei@umich.edu>
2021-01-12 00:06:26 -0500

    change lib.loc

--- extdata/step2_SPAtests.R
+++ extdata/step2_SPAtests.R
@@ -1,8 +1,9 @@
 #!/usr/bin/env Rscript
 
 options(stringsAsFactors=F)
+library(SAIGE)
 #library(SAIGE, lib.loc="/net/hunt/zhowei/project/imbalancedCaseCtrlMixedModel/Rpackage_SPAGMMAT/installSAIGEFolder/0.40.2_Surv_LOCO_debug")
-library(SAIGE, lib.loc="/net/hunt/zhowei/project/imbalancedCaseCtrlMixedModel/Rpackage_SPAGMMAT/installSAIGEFolder/0.40.2_Surv_LOCO_debug/")
+#library(SAIGE, lib.loc="/net/hunt/zhowei/project/imbalancedCaseCtrlMixedModel/Rpackage_SPAGMMAT/installSAIGEFolder/0.40.2_Surv_LOCO_debug/")
 #library(SAIGE, lib.loc="/humgen/atgu1/fin/wzhou/tools/SAIGE_Poission_Survival/install_Poisson_Survival_0.36.6.1")
 #library(SAIGE, lib.loc="/humgen/atgu1/fin/wzhou/tools/SAIGE_Poission_Survival/install_Poisson_Survival_0.36.6")
 #library(SAIGE, lib.loc="~/install_dir/0.36.3.5_homN_hetN_surv")
