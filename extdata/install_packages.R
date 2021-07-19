8f7c45c08ef8cee9ce4c3e314ab32b67b3b828f9
weizhouUMICH <zhowei@umich.edu>
2021-07-19 15:25:08 -0400

    install dependent R libraries

--- extdata/install_packages.R
+++ extdata/install_packages.R
@@ -1,10 +1,12 @@
 #!/usr/bin/env Rscript
 #install required R packages, from Finnge/SAIGE-IT
 
-req_packages <- c("R.utils", "Rcpp", "RcppParallel", "RcppArmadillo", "data.table", "RcppEigen", "Matrix", "methods", "BH", "optparse", "SPAtest", "MetaSKAT", "SKAT", "roxygen2", "rversions","devtools", "MASS")
+req_packages <- c("R.utils", "Rcpp", "RcppParallel", "RcppArmadillo", "data.table", "RcppEigen", "Matrix", "methods", "BH", "optparse", "SPAtest", "SKAT", "roxygen2", "rversions","devtools", "MASS")
 for (pack in req_packages) {
     if(!require(pack,character.only = TRUE)) {
         install.packages(pack, repos = "http://cran.us.r-project.org")
     }
 }
 
+
+devtools::install_github("leeshawn/MetaSKAT")
